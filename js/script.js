1d827332a30b7d0711cf15d29056730b069079f7
HOME-PC\Hennadii <dmtrnknatalia.3@gmail.com>
2021-02-09 16:54:45 +0200

    Work with JS

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//Тут могут быть только комментарии,строгий режим(use strict) пишется всегда в 1-ой строке
+'use strict';
+let currentUserName = prompt('Your name?');
+
+alert(currentUserName);
+
+alert(`your name are ${prompt('What\'s your name?','')}`);
