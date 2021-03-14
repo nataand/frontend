d27e0e8cde85574a9354f76fc5521b6d94a55158
HOME-PC\Hennadii <dmtrnknatalia.3@gmail.com>
2021-03-14 12:13:51 +0200

    Some change

--- js/script.js
+++ js/script.js
@@ -1,7 +1,96 @@
 //Тут могут быть только комментарии,строгий режим(use strict) пишется всегда в 1-ой строке
 'use strict';
-let currentUserName = prompt('Your name?');
+// let currentUserName = prompt('Your name?');
+// alert(currentUserName);
+// alert(`your name are ${prompt('What\'s your name?','')}`);
 
-alert(currentUserName);
+//let year = prompt("В каком году родилась?");
 
-alert(`your name are ${prompt('What\'s your name?','')}`);
+// if (year == 1998) { 
+//     alert("верно");
+//     alert("Great!");
+// }else {
+//     alert("А вот и не правильно!");
+// }
+
+
+// let year = prompt("В каком году родилась?");
+
+// if (year < 1998){
+//     alert("Слишком рано");
+// }else if (year > 1998){
+//     alert("Поздновато...");
+// }
+
+// let result = prompt("Введите число");
+
+// let number = (result > 0) ? "1" :
+// (result < 0) ? "-1" :
+// (result == 0) ? "0" :
+// "Фігня якась";
+
+// alert(number);
+
+// let result = prompt("Введите числа");
+
+// result = (a + b < 4) ? "Мало" : "Много";
+
+// let message = prompt("Введите логин");
+// let login = "Сотрудник";
+// messаge = (login == "Сотрудник") ? "Привет" :
+// (login == "Директор") ? "Здравствуйте" :
+// (login == " ") ? "Нет логина" : "";
+
+// let age = prompt("How old are you?", "");
+// let massage = (age < 14) ? "false" :
+// (age >= 14) ? "true" :
+// (age <= 90) ? "true" :
+// (age > 90) ? "false" : " ";
+
+// let age = prompt("How old are you?", "");
+// alert("true");
+
+// if (age >= 14 && age <= 90);
+
+
+
+// let login = prompt("Enter your login");
+
+// if (login == "Admin") {
+//     let pass = prompt("Enter your password");
+//     if (pass == "pass") {
+//         alert("Hello " + login);
+//     } else {
+//         alert("Invalid password");
+//     }
+// } else if (login == "" || login == null) {
+//     alert("Invalid login");
+// } else {
+//     alert("Can't find user");
+// }
+
+
+
+
+
+
+
+// if (login == "Admin") {
+//     let password = prompt("Your password");
+//     if (password == "Im boss") {
+//         alert("Hello!")
+//     }
+// } else if (login == "" || login == "null") {
+//     alert("I don't now who are you...");
+    
+// } else if (password == "" || password == "null") {
+//     alert("Wrong password");
+// }
+
+// let age = 0;
+
+// if (age >= 14 && age <= 90) {
+//     console.log('hello world');
+// } else {
+//     console.log('dfgdfgdfg world');
+// }
