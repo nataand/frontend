048af7e4e4fe1127f8b524510f04fe045ef86066
HOME-PC\Hennadii <dmtrnknatalia.3@gmail.com>
2021-03-17 20:02:46 +0200

    Some changes

--- js/script.js
+++ js/script.js
@@ -94,3 +94,127 @@
 // } else {
 //     console.log('dfgdfgdfg world');
 // }
+
+
+// for (let i = 0; i < 3; i++) {
+//     alert(i);
+// }
+
+
+// let a = 2 + 2;
+
+// switch (a) {
+//     case 3:
+//         alert( "Маловато" );
+//         break;
+//     case 4:
+//         alert( "В точку!" );
+//         break;
+//     case 5:
+//         alert( "Перебор" );
+//         break;
+//     default:
+//         alert( "Нет таких значений" );            
+// }
+
+
+// let arg = prompt("Введите число");
+// switch (arg) {
+//     case "0":
+//     case "1":
+//         alert("Один или ноль");
+//         break;
+        
+//     case "2":
+//         alert("Два");
+//         break;
+        
+//     case "3":
+//         alert("Никогда не выполнится");
+//         break;
+//     default:
+//         alert("Неизвестное значение");        
+// }
+
+
+
+// let browser = prompt("Каким браузером вы пользуетесь?,");
+
+// if (browser == "Edge") {
+//     alert("You've got the Edge!")
+// } else if (browser == "Chrome" 
+// || browser == "Firefox"
+// || browser == "Safari" 
+// || browser == "Opera") {
+//     alert("Okay we support these browsers too");
+// } else {
+//     alert("We hope that this page looks ok!");
+// }
+
+
+
+
+
+// const number = +prompt('Введите число между 0 и 3', '');
+
+// switch (number) {
+//     case 0:
+//         alert("Вы ввели число 0");
+//         break;
+
+//     case 1:
+//         alert("Вы ввели число 1");
+//         break;
+        
+//     case 2:
+//     case 3:    
+//         alert("Вы ввели число 2, а может и 3");
+//         break;
+//     default:
+//         alert("Other number");        
+// }
+
+
+
+
+// function min(a,b) {
+//     if (a < b) {
+//         return a;
+//     } else {
+//         return b;
+//     }
+// }
+
+
+// function min(a,b) {
+//     return a < b ? a : b;
+// }
+
+
+
+
+
+
+
+
+
+
+
+let i = 0;
+// while (i < 5) { 
+//   alert( i );
+//   i++;
+// }
+
+
+// do {
+//     alert(i);
+//     i++;
+// } while (i < 5);
+
+
+for(;;) {
+    let somePrompt = +prompt("Введите число","")
+        if (somePrompt > 100 || break);
+    
+  }
